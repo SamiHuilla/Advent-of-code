85355108dd0f21664c2064336eb920fd14a93f82
Sami Huilla <sami.huilla@hotmail.com>
2015-12-24 11:15:32 +0200

    day5 final

--- day5.py
+++ day5.py
@@ -27,9 +27,38 @@ def hasPair(string):
 	index = 0
 	for index, char in enumerate(string):
 		if(string.count(string[index:index+2])>=2):
-			return True
+			if(index>=2):
+				if(string[index]==string[index-1]==string[index-2]):
+					return None
+			else:
+				return True
 		
 	return None
+
+def hasBurger(string):
+	index = 0
+	for index, char in enumerate(string):
+		if(index>=2):
+			if(string[index]==string[index-2]):
+				return True
+	return None
+
+def is_really_nice(s):
+    first = False
+    for i in range(len(s) - 3):
+        sub = s[i: i + 2]
+        if sub in s[i + 2:]:
+            first = True
+            print("{} is really nice and repeats with {}".format(s, sub))
+            break
+    if not first:
+        return False
+    second = False
+    for i in range(len(s) - 2):
+        if s[i] == s[i + 2]:
+            second = True
+            break
+    return second
 
 nice = 0
 string = ""
@@ -37,7 +66,7 @@ with open('input5', encoding='utf-8') as input:
     for line in input:
     	string = str(line)
     	#if (hasVows(string) and hasRow(string) and noStrings(string)): #PART 1
-    	if (hasPair(string)): 
+    	if (is_really_nice(string)): 
     		nice += 1
 
 print(nice)
